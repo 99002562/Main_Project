676e5ec6ada422fdb5c9eb9eefb081ac36fe9573
99002562 <71489187+99002562@users.noreply.github.com>
2020-12-07 14:35:54 +0530

    Update CalculatorSpec.js

--- src/CalculatorSpec.js
+++ src/CalculatorSpec.js
@@ -95,5 +95,31 @@ function Clr(){
 
 
 
+//SpecHelper
+
+beforeEach(function () {
+  jasmine.addMatchers({
+    toBePlaying: function () {
+      return {
+        compare: function (actual, expected) {
+          var player = actual;
+
+          return {
+            pass: player.currentlyPlayingSong === expected && player.isPlaying
+          };
+        }
+      };
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
 
 
